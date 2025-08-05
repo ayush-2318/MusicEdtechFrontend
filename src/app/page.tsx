50f378ced847b1c3ae8ef8709dd0d519f6c67bc5
Ayush Kumar <golutpr@gmail.com>
2025-08-06 00:08:02 +0530

    spotlight done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     
    <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[.02]">
       <h1 className="text-center" >Hello World !</h1>
+      <HeroSection/>
    </main>
    
     
